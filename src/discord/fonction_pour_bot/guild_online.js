8c240d03eee1b17e393ac2f141c001825174aa81
Jason54 <33364110+Jason54jg@users.noreply.github.com>
2023-02-27 15:08:26 +0100

    Update guild_online.js

--- src/discord/fonction_pour_bot/guild_online.js
+++ src/discord/fonction_pour_bot/guild_online.js
@@ -1,19 +1,18 @@
-const guild_list=require("../../../API/functions/getguildlist")
-const {EmbedBuilder}=require("discord.js");
+const guild_list = require("../../../API/functions/getguildlist")
+const { EmbedBuilder } = require("discord.js");
 const { cp } = require("fs");
 
 
 
-module.exports={guild_online}
+module.exports = {guild_online}
 
-async function guild_online(guild){
-    let embed= new EmbedBuilder()
-    if (guild==undefined){
+async function guild_online(guild) {
+    let embed = new EmbedBuilder()
+    if (guild == undefined){
         embed.setColor(0x099FF)
         embed.setTitle("Erreur")
         embed.setDescription("vous devez spécifiez une guild avec l'options guild")
-    }
-    else{
+    } else {
     const transition=await guild_list.guild_list(guild);
     const guild_liste=transition[0]
     const k=guild_liste.length
@@ -21,48 +20,43 @@ async function guild_online(guild){
     const liste_rank_sanstag=transition[2]
     const liste_ranks_affichage=transition[3]
     const en_ligne=transition[4];
-    /*let à_afficher="";
-    for (let i=0;i<guild_liste.length;i++){
-      à_afficher+=guild_liste[i];
-      à_afficher+='\n'
-    }
-    */
       embed.setColor(0x0099ff)
       embed.setTitle("Membres de la guild")
     for (let i=0;i<liste_rank_sanstag.length;i++){
-        let rank_affich=liste_rank_sanstag[i]
-        let a_afficher=""
-        let a_afficher1=""
-        let b=0
-        let c=0;
+        let rank_affich = liste_rank_sanstag[i]
+        let a_afficher = ""
+        let a_afficher1 = ""
+        let b = 0
+        let c = 0;
         for (let j=0;j<liste_member_rank.length;j++){
-            let member_rank=liste_member_rank[j]
-            if (member_rank==rank_affich){
-                if (en_ligne[j]==false){
+            let member_rank = liste_member_rank[j]
+            if (member_rank == rank_affich){
+                if (en_ligne[j] == false){
                 a_afficher+="``"+guild_liste[j]+"❌`` "}
-                else{
+                else {
                   if (en_ligne[j]=="?"){
                     a_afficher+="``"+guild_liste[j]+"❓`` "}
-                  else{
+                  else {
                   a_afficher+="``"+guild_liste[j]+"✔️`` "}}
                 b+=1
-                if (b>50){
-                  if (c==0){
-                    c=1
-                    a_afficher1=a_afficher
-                a_afficher=""
+                if (b>50) {
+                  if (c==0) {
+                    c = 1
+                    a_afficher1 = a_afficher
+                a_afficher = ""
               }
-         }}}
-        //embed.addFields()
-        //try{
-        if (c==0){
+         }
+        }
+       }
+        if (c == 0){
         embed.addFields({
-            name:liste_ranks_affichage[i]+" >> "+b.toString(),
-            value:a_afficher,
-        })}
-        else{
+            name: liste_ranks_affichage[i]+" » "+b.toString(),
+            value: a_afficher,
+        })
+       }
+        else {
           embed.addFields({
-            name:liste_ranks_affichage[i]+" >> "+b.toString(),
+            name:liste_ranks_affichage[i]+" » "+b.toString(),
             value:a_afficher1,
             inline:false
         })
@@ -72,13 +66,8 @@ async function guild_online(guild){
             inline:true
           })
         }
-          //}
-        //catch (error){
-          //console.log(error)
-          //console.log("error")
-        //}
     }
-    embed.setDescription("Guild Member count : **"+k.toString()+"/125**")
+    embed.setDescription("Nombre de membres de la guilde : **"+k.toString()+"/125**")
     }
 
     return(embed)
