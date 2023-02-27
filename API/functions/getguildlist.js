5343c1c8694dec78ede819573ce2af09c17e2c33
mathferm <126267041+mathferm@users.noreply.github.com>
2023-02-27 14:29:26 +0100

    Update getguildlist.js

--- API/functions/getguildlist.js
+++ API/functions/getguildlist.js
@@ -1,3 +1,4 @@
+const { cp } = require('fs');
 const https = require('https');
 const config = require("../../config.json");
 const requete= require("./requete") 
@@ -7,6 +8,9 @@ const Mojang="https://sessionserver.mojang.com/session/minecraft/profile/";
 const api_key=config.minecraft.API.hypixelAPIkey;
 const hypixel_api=config.minecraft.API.hypixelAPI;
 
+async function delay(ms){
+  return await new Promise(resolve=>setTimeout(resolve,ms));
+}
 
 function get_pagedeux(page){
     const https = require('https');
@@ -44,14 +48,56 @@ async function guild_list(guild){
     }
     const membre=guildeee.members;
     let liste_uuid=[];
+    let rankss=[]
+    let rankssanstag=[]
+    let num=[]
+    let liste_member_rank=[];
+    let onlines=[]
     for (let i=0;i<membre.length;i++){
       liste_uuid.push(membre[i].uuid);
+      let p=await requete.get_page(hypixel_api+"/status"+"?key="+api_key+"&uuid="+membre[i].uuid)
+      try{
+        let onli=p.session.online
+
+      onlines.push(p.session.online)
+      liste_member_rank.push(membre[i].rank)}
+      catch (error){
+        onlines.push("?")
+        const a= await delay(10000)
+      }
     }
+    let_ranks_non_tries=guildeee["ranks"];
+    for (let i=0;i<let_ranks_non_tries.length;i++){
+      let numero=let_ranks_non_tries[i].priority
+      let nom_rank_provi=let_ranks_non_tries[i].name
+      let tag_rank=let_ranks_non_tries[i].tag
+      let nom_rank=nom_rank_provi+" ["+tag_rank+"]"
+      let a=0
+      for (let j=0;j<num.length;j++){
+        console.log(num,j,i)
+        if (num[j]<numero){
+          if (a==0){
+          num.splice(j,0,numero)
+          rankss.splice(j,0,nom_rank)
+          rankssanstag.splice(j,0,nom_rank_provi)
+          a=1
+          j+=1}
+        }}
+      if (a==0){
+        num.push(numero)
+        rankss.push(nom_rank)
+        rankssanstag.push(nom_rank_provi)
+      }
+    }
+    console.log(num)
+    console.log("num")
+    rankss.splice(0,0,'Guild Master [GM]')
+    rankssanstag.splice(0,0,"Guild Master")
     console.log(liste_uuid);
     console.log("c'est la liste des uuid")
-    let guild_liste= await uuid_username.uuid_username(liste_uuid)
+    let guild_liste=await uuid_username.uuid_username(liste_uuid)
     console.log(guild_liste);
     console.log("C la guild liste")
-    return(guild_liste);
-}
+    return([guild_liste,liste_member_rank,rankssanstag,rankss,onlines]);
+  }
 module.exports={guild_list};
