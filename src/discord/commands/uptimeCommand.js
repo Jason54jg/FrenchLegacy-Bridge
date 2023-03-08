f5e13e9d1ab456455d936bee849d065ab64be7ac
A6sT <ewan.chauvin67000@gmail.com>
2023-03-08 07:13:29 -0800

    Fix de la commande uptime

--- src/discord/commands/uptimeCommand.js
+++ src/discord/commands/uptimeCommand.js
@@ -3,19 +3,18 @@ const { toFixed } = require("../../contracts/helperFunctions.js");
 const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
-  name: "uptime",
-  description: "Affiche la disponibilité du bot.",
+    name: "uptime",
+    description: "Affiche la disponibilité du bot.",
 
-  execute: async (interaction, client) => {
-    const uptimeEmbed = new EmbedBuilder()
-      .setColor(0x0099ff)
-      .setTitle("🕐 Uptime!")
-      .setDescription(`En ligne depuis <t:${toFixed(client.uptime / 1000, 0)}:R>`)
-      .setFooter({
-        text: `/help [command] pour plus d'informations`,
-        iconURL: "https://media.discordapp.net/attachments/1073744026454466600/1076983462403264642/icon_FL_finale.png",
-      });
-
-    interaction.reply({ embeds: [uptimeEmbed] });
-  },
+    execute: async (interaction, client) => {
+        const uptimeEmbed = new EmbedBuilder()
+            .setColor(0x0099ff)
+            .setTitle("🕐 Uptime!")
+            .setDescription(`En ligne depuis <t:${+ toFixed((Date.now() + client.uptime) / 1000, 0)}:R>`)
+            .setFooter({
+                text: `/help [command] pour plus d'informations`,
+                iconURL: "https://media.discordapp.net/attachments/1073744026454466600/1076983462403264642/icon_FL_finale.png",
+            });
+        interaction.reply({ embeds: [uptimeEmbed] });
+    },
 };
