0fb4b059d1ff4fd5256eaa0b0c86b96dc9f229e5
mathferm <126267041+mathferm@users.noreply.github.com>
2023-09-17 17:32:48 +0200

    Update DiscordManager.js
    Normalement ça règle le pb

--- src/discord/DiscordManager.js
+++ src/discord/DiscordManager.js
@@ -171,7 +171,7 @@ class DiscordManager extends CommunicationBridge {
   async onBroadcast({ fullMessage, chat, chatType, username, rank, guildRank, message, color = 1752220 }) {
     if (
       (chat === undefined && chatType !== "debugChannel") ||
-      ((username === undefined || message === undefined) && chat !== "debugChannel") || (username==config.minecraft.bot.pseudo1 && "»" in fullMessage)
+      ((username === undefined || message === undefined) && chat !== "debugChannel") || (username==config.minecraft.bot.pseudo1 && fullMessage.includes("»"))
     ) {
       return;
     }
